2872ac93b95facb09aab0329d898f2133965b56d
Raul Espinola <raulespinola@gmail.com>
2019-02-22 11:40:04 -0300

    GameSteam | ItemUser Relation | Add SQL data  #17

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
 INSERT INTO User (id, creation_date, delete_date, edition_date, version, email, name, customer_id) VALUES 
 (1, NULL, NULL, NULL, 1, 'Monchi', 'hola@gmail', NULL);
+
+INSERT INTO User (id, creation_date, delete_date, edition_date, version, email, name, customer_id) VALUES 
+(5, NULL, NULL, NULL, 1, 'Juancito', 'juan@gmail', NULL);
