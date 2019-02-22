87d5cec55c2194e64bfcb6effb8bb080aab51f01
Raul Espinola <raulespinola@gmail.com>
2019-02-22 15:35:12 -0300

    GamesSteam | DEV | Add and Update Game, Mod and Description Services |  #18

--- src/main/java/com/springmvc/services/DescriptionService.java
+++ src/main/java/com/springmvc/services/DescriptionService.java
@@ -2,6 +2,9 @@ package com.springmvc.services;
 
 import com.springmvc.entities.Description;
 
+/*
+ * This is a comment
+ */
 public interface DescriptionService extends CRUDService<Description, Long> {
 
 }
