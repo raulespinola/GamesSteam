a986ec59706579ed4e5627550a5626066f6f1b6b
Raul Espinola <raulespinola@gmail.com>
2019-02-22 12:36:06 -0300

    GamesSteam | SQLData | Add new data #17

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,20 @@
+/* User */
+
 INSERT INTO User (id, creation_date, delete_date, edition_date, version, email, name, customer_id) VALUES 
 (1, NULL, NULL, NULL, 1, 'Monchi', 'hola@gmail', NULL);
 
 INSERT INTO User (id, creation_date, delete_date, edition_date, version, email, name, customer_id) VALUES 
 (5, NULL, NULL, NULL, 1, 'Juancito', 'juan@gmail', NULL);
+
+INSERT INTO User (id, creation_date, delete_date, edition_date, version, email, name, customer_id) VALUES 
+(9, NULL, NULL, NULL, 1, 'Joseson', 'juan@gmail', NULL);
+
+
+
+/* Description */
+INSERT INTO DESCRIPTION (id, creation_date, delete_date, edition_date, version, mobiledescription, webdescription) VALUES 
+(1, NULL, NULL, NULL, 1, 'Diablo 2 Mobile', 'Diablo 2 Web');
+
+INSERT INTO DESCRIPTION (id, creation_date, delete_date, edition_date, version, mobiledescription, webdescription) VALUES 
+(2, NULL, NULL, NULL, 1, 'Diablo Mod Exp Mobile', 'Diablo Mod Exp Web');
+
